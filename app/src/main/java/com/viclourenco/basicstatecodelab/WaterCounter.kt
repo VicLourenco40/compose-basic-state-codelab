59a9b26aea7283056a4050be72f2dc0d6c7acba9
VicLourenco <134103080+VicLourenco40@users.noreply.github.com>
2025-01-02 19:19:26 +0000

    5. Memory in a composable function

--- app/src/main/java/com/viclourenco/basicstatecodelab/WaterCounter.kt
+++ app/src/main/java/com/viclourenco/basicstatecodelab/WaterCounter.kt
@@ -1,24 +1,29 @@
 package com.viclourenco.basicstatecodelab
 
+import android.annotation.SuppressLint
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 
 @Composable
 fun WaterCounter(modifier: Modifier = Modifier) {
-    var count = 0
+    var count by remember { mutableStateOf(0) }
 
     Column(modifier = modifier.padding(16.dp)) {
-        Text(text = "You've had $count glasses.")
+        Text("You've had $count glasses.")
         Button(
             onClick = { count++ },
             modifier = Modifier.padding(top = 8.dp)
         ) {
-            Text(text = "Add one")
+            Text("Add one")
         }
     }
 }
